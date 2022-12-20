bc15febf4d29847713fab97a62c712cf5b9a24f4
Ligia Falcao <lih.falcao@hotmail.com>
2022-12-20 08:37:38 -0300

    correções

--- store/src/app/login/login.component.ts
+++ store/src/app/login/login.component.ts
@@ -37,7 +37,7 @@ export class LoginComponent implements OnInit {
   }
 
   register(){
-    this.router.navigate(["/register"]);
+    this.router.navigate(["/registrar"]);
   }
 
   submit(data:any) {
